599633d77941dbac9f3bb558b92f4903498b985f
AMRUSIR <92585914+AMRUSIR@users.noreply.github.com>
2022-08-03 19:40:52 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -55,22 +55,22 @@ global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
 global.vcardowner = ['916909137213'] //ur owner number
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
+global.ownername = "AMRUSER" //ur owner name
+global.ytname = "YT: DRON" //ur yt chanel name
 global.socialm = "GitHub: DGXeon" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //bot bomdy 
-global.owner = ['916909137213']
-global.ownertag = '916909137213' //ur tag number
-global.botname = 'Cheems Bot MD V4' //ur bot name
+global.owner = ['919895802105']
+global.ownertag = '919895802105' //ur tag number
+global.botname = 'AMRU-SER-MD' //ur bot name
 global.linkz = "https://youtu.be/WiIqCdiDjFo" //your theme url which will be displayed on whatsapp
 global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
 global.botscript = 'https://github.com/DGXeon/CheemsBot-MD4' //script link
-global.reactmoji = "😘" //ur menu react emoji
-global.themeemoji = "🦄" //ur theme emoji
+global.reactmoji = "🙂" //ur menu react emoji
+global.themeemoji = "😈" //ur theme emoji
 global.packname = "AMRU\n\n\n\n\n\n\n\n\n\n\n😈😈😈 " //ur sticker watermark packname
-global.author = "🦄Dream Guy Xeon" //ur sticker watermark author
+global.author = "AMRU-SER-MD" //ur sticker watermark author
 
 //Bot theme media
 global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
