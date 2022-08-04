7558d502651ecc0048338dd0f1e64dc4fc4769a6
AMRUSIR <92585914+AMRUSIR@users.noreply.github.com>
2022-08-04 20:22:44 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -37,9 +37,9 @@ global.APIKeys = {
 
 //aumto functioner
 global.autoTyping = false //auto tying in gc (true to on, false to off)
-global.autoreadpmngc = false //auto reading in gc and pm (true to on, false to off)
-global.autoReadGc = false //auto reading in gc (true to on, false to off)
-global.autoRecord = false //auto recording (true to on, false to off)
+global.autoreadpmngc = true //auto reading in gc and pm (true to on, false to off)
+global.autoReadGc = true //auto reading in gc (true to on, false to off)
+global.autoRecord = true //auto recording (true to on, false to off)
 global.available = true //auto available (true to on, false to off)
 
 //semt menu variants
